5d420b7e94c321f0457bf9f9821d54fced82d554
samiran <sam.de.721166@gmail.com>
2026-06-03 00:34:48 +0530

    adding chart

--- frontend/src/components/analytics.jsx
+++ frontend/src/components/analytics.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
 
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "../components/shadcn/chart"
+
+import { BarChart, Bar, XAxis } from "recharts"
+
+const data = [
+  { month: "Jan", value: 100 },
+  { month: "Feb", value: 200 },
+]
+
+
 function Analytics() {
   return (
-    <div className='bg-neutral-900 p-5 w-screen h-screen text-white justify-center flex'>
-        <p>Analytics</p>
+    <div className='bg-neutral-900 p-10 h-screen w-screen'>
+      <ChartContainer
+        className="h-[300px] w-50 "
+        config={{
+          value: {
+            label: "Time",
+            color: "var(--chart-1)",
+          },
+        }}
+      >
+        <BarChart data={data}>
+          <XAxis dataKey="month" />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <Bar dataKey="value" fill="var(--color-value)"  />
+        </BarChart>
+      </ChartContainer>
     </div>
   )
 }
