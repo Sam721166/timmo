50c7558b525145526cfc373a425b37a5777e0fb1
samiran <sam.de.721166@gmail.com>
2026-06-11 23:06:10 +0530

    adding leaderboard

--- frontend/src/components/leaderboard/leaderboard.jsx
+++ frontend/src/components/leaderboard/leaderboard.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import List from "../leaderboard/list"
+import { FaCrown, FaFire } from "react-icons/fa6";
+
 
 function Leaderboard() {
   return (
-    <div className='w-screen h-screen bg-neutral-900  justify-center flex pt-10 font-poppins'>
-        <div className='w-7xl min-w-0 h-full flex gap-2 flex-col'>
+    <div className='w-screen h-screen bg-neutral-900  justify-center flex pt-10 font-poppins overflow-y-scroll '
+    style={{
+        scrollbarWidth: "thin",
+        scrollbarColor: "gray transparent",
+    }} >
+        <div className='w-7xl min-w-0 h-full flex gap-2 flex-col ' >
             
             <div className='flex flex-col gap-2 border-b border-white/10 pb-5  h-15 w-7xl '>
                 <div className='flex flex-col justify-between gap-3 sm:flex-row sm:items-end pb-9'>
@@ -17,15 +23,226 @@ function Leaderboard() {
             </div>
 
 
-            <div className='w-full  mt-2'>
+            <div className='w-250 flex justify-between items-center gap-3 mt-2 '>
                 <div className=' border-2 border-white/10 rounded-lg w-35 h-10.5 cursor-pointer active:scale-99 transition-all duration-200 bg-neutral-900 flex justify-center items-center'>
                     <p className='font-poppins tracking-tight px-3 py-1 border border-white/20 rounded-sm w-33 bg-white/10 '>Last 24 hours</p>
                 </div>
+
+                <div className='text-sm text-neutral-500 border-2 font-poppins bg-white/2 border-white/10 rounded-sm px-3 tracking-tight font-semibold py-1 flex gap-2 items-center justify-center'>
+                    <div className='bg-green-500 text-xl rounded-full size-2'></div>
+                    <p>Updates every 5 minutes</p>
+                </div>
+                
             </div>
 
 
-            <div>
-                <List />
+
+
+            
+            <div className='w-full h-full bg-neutral-900 '>
+            
+            
+                    <div className='rounded-md border-2 border-white/10 w-250 h-auto   mt-2 '>
+            
+                        <div className='border-b-2 border-white/10 gap-10 font-poppins text-sm text-neutral-500 bg-white/5 rounded-t-md px-10 py-2 items-center flex  justify-between h-10 w-full  '>
+            
+                            <div className='flex gap-10 mr-70'>
+                                <p>Rank</p>
+                                <p>Name</p> 
+                            </div>
+                            
+                            <p>Today's time</p>
+                            <p className='mr-10'>Streak</p>
+            
+                        </div>
+            
+            
+                        <div className='w-full h-auto   flex flex-col pb-1 overflow-hidden'
+                        >
+                                
+                            <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 w-full min-h-16 items-center grid grid-cols-[80px_1fr_195px_135px] ">
+            
+            
+                                {/* Rank */}
+                                <div className='rounded-full text-xs size-8 border-2 border-yellow-500 bg-gradient-to-r
+                                from-amber-500/15
+                                via-yellow-500/10
+                                to-transparent flex items-center justify-center font-bold shadow-[0_0_10px_0px] shadow-yellow-400 '
+                                style={{
+                                    animation: 'glow 3s ease-in-out infinite'
+                                }}>
+                                    <span>🥇</span>
+                                    <span className=' mr-1 '>1</span>
+                                </div>
+            
+                                {/* User */}
+                                <div className="flex items-center gap-2 min-w-0">
+                                    <div className="rounded-full size-8 bg-red-500/30 flex items-center justify-center text-xl">
+                                    <p>N</p>
+                                    </div>
+            
+                                    <div className='flex flex-col '>
+                                        <p className="truncate">
+                                            Nigga
+                                        </p>
+                                        <p className="text-xs text-yellow-400">
+                                            Has No Life
+                                        </p>
+                                    </div>
+                                    
+                                    
+                                </div>
+            
+                                {/* Time */}
+                                <div className=''>
+                                    <p>7h 23m</p>
+                                </div>
+            
+                                {/* Streak */}
+                                <div className="flex items-center gap-1  justify-center ">
+                                    <FaFire className="text-white" />
+                                    <span>67 days</span>
+                                </div>
+                            </div>
+                                
+                            <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 w-full min-h-16 items-center grid grid-cols-[80px_1fr_195px_135px] ">
+            
+            
+                                {/* Rank */}
+                                <div className='rounded-full text-xs size-8  flex items-center border-2 justify-center font-bold bg-zinc-400/5
+                                border-zinc-300/40
+                                shadow-[0_0_20px_rgba(212,212,216,0.08)]'
+                                style={{
+                                    animation: 'silverGlow 3s ease-in-out infinite'
+                                }}>
+                                    <span>🥈</span>
+                                    <span className='mr-1'>2</span>
+                                </div>
+            
+                                {/* User */}
+                                <div className="flex items-center gap-2 min-w-0">
+                                    <div className="rounded-full size-8 bg-purple-400/30 flex items-center justify-center text-xl">
+                                    <p>R</p>
+                                    </div>
+            
+                                    <div className='flex flex-col '>
+                                        <p className="truncate">
+                                            Ram
+                                        </p>
+                                        <p className="text-xs text-slate-400 font-medium">
+                                            Touch Grass
+                                        </p>
+                                    </div>
+                                    
+                                    
+                                </div>
+            
+                                {/* Time */}
+                                <div className=''>
+                                    <p>4h 13m</p>
+                                </div>
+            
+                                {/* Streak */}
+                                <div className="flex items-center gap-1  justify-center ">
+                                    <FaFire className="text-white" />
+                                    <span>12 days</span>
+                                </div>
+                            </div>
+            
+                                
+                            <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 w-full min-h-16 items-center grid grid-cols-[80px_1fr_195px_135px] ">
+            
+            
+                                {/* Rank */}
+                                <div className='rounded-full text-xs size-8  flex items-center border-2 justify-center font-bold bg-orange-500/5
+                                border-orange-400/40
+                                shadow-[0_0_20px_rgba(251,146,60,0.08)]'
+                                style={{
+                                    animation: 'bronzeGlow 3s ease-in-out infinite'
+                                }}>
+                                    <span>🥉</span>
+                                    <span className='mr-1'>3</span>
+                                </div>
+            
+                                {/* User */}
+                                <div className="flex items-center gap-2 min-w-0">
+                                    <div className="rounded-full size-8 bg-yellow-400/30 flex items-center justify-center text-xl">
+                                    <p>J</p>
+                                    </div>
+            
+                                    <div className='flex flex-col '>
+                                        <p className="truncate">
+                                            Jack
+                                        </p>
+                                        <p className="text-xs text-orange-400/80">
+                                           Locked In
+                                        </p>
+                                    </div>
+                                    
+                                    
+                                </div>
+            
+                                {/* Time */}
+                                <div className=''>
+                                    <p>2h 3m</p>
+                                </div>
+            
+                                {/* Streak */}
+                                <div className="flex items-center gap-1  justify-center ">
+                                    <FaFire className="text-white" />
+                                    <span>3 days</span>
+                                </div>
+                            </div>
+                                
+                            <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 w-full min-h-16 items-center grid grid-cols-[80px_1fr_195px_135px] ">
+            
+            
+                                {/* Rank */}
+                                <div className='rounded-full text-xs size-8  flex items-center  justify-center font-bold bg-white/5
+                                border-white/20'>
+                                    <span className=''>4</span>
+                                </div>
+            
+                                {/* User */}
+                                <div className="flex items-center gap-2 min-w-0">
+                                    <div className="rounded-full size-8 bg-blue-400/30 flex items-center justify-center text-xl">
+                                    <p>M</p>
+                                    </div>
+            
+                                    <div className='flex flex-col '>
+                                        <p className="truncate">
+                                            Mike
+                                        </p>
+                                    </div>
+                                    
+                                    
+                                </div>
+            
+                                {/* Time */}
+                                <div className=''>
+                                    <p>1h 46m</p>
+                                </div>
+            
+                                {/* Streak */}
+                                <div className="flex items-center gap-1  justify-center ">
+                                    <FaFire className="text-white" />
+                                    <span>1 days</span>
+                                </div>
+                            </div>
+            
+            
+            
+                            
+                            <div className='px-10 w-full h-20 font-poppins text-neutral-500 text-sm flex items-center justify-center gap-1 flex-col '>
+                                <p>Showing top 100 users.</p>
+                                <p className='text-neutral-600'>Procrastinators were filtered out.</p>
+                            </div>
+            
+                        </div>
+            
+            
+                    </div>
+            
             </div>
             
 
--- frontend/src/components/leaderboard/list.jsx
+++ frontend/src/components/leaderboard/list.jsx
@@ -8,9 +8,9 @@ function List() {
     <div className='w-full h-full bg-neutral-900 '>
 
 
-        <div className='rounded-md border-2 border-white/10 w-250 h-screen   mt-2 '>
+        <div className='rounded-md border-2 border-white/10 w-250 h-auto   mt-2 '>
 
-            <div className='border-b-2 border-white/10 flex  gap-10 font-poppins text-sm text-neutral-500 bg-white/5 rounded-t-md px-10 py-2 items-center justify-between h-10 w-full  '>
+            <div className='border-b-2 border-white/10 gap-10 font-poppins text-sm text-neutral-500 bg-white/5 rounded-t-md px-10 py-2 items-center flex  justify-between h-10 w-full  '>
 
                 <div className='flex gap-10 mr-70'>
                     <p>Rank</p>
@@ -23,14 +23,14 @@ function List() {
             </div>
 
 
-            <div className='w-full h-screen overflow-scroll flex flex-col pb-1'
+            <div className='w-full h-140  overflow-y-scroll flex flex-col pb-1 '
                 style={{
                     scrollbarWidth: "thin",
                     scrollbarColor: "gray transparent",
                 }} 
             >
                     
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
+                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 w-full min-h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
 
 
                     {/* Rank */}
@@ -62,280 +62,12 @@ function List() {
                 </div>
 
 
-                    
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
-
-
-                    {/* Rank */}
-                    <div>
-                        <p># 2</p>
-                    </div>
-
-                    {/* User */}
-                    <div className="flex items-center gap-2 min-w-0">
-                        <div className="rounded-full size-7 bg-violet-500/30 flex items-center justify-center text-xl">
-                        <p>M</p>
-                        </div>
-
-                        <p className="truncate">
-                        Mike Jackson
-                        </p>
-                    </div>
-
-                    {/* Time */}
-                    <div className=''>
-                        <p>4h 43m</p>
-                    </div>
-
-                    {/* Streak */}
-                    <div className="flex items-center gap-1  justify-center ">
-                        <FaFire className="text-white" />
-                        <span>12 days</span>
-                    </div>
-                </div>
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
-
-
-                    {/* Rank */}
-                    <div>
-                        <p># 2</p>
-                    </div>
-
-                    {/* User */}
-                    <div className="flex items-center gap-2 min-w-0">
-                        <div className="rounded-full size-7 bg-violet-500/30 flex items-center justify-center text-xl">
-                        <p>M</p>
-                        </div>
-
-                        <p className="truncate">
-                        Mike Jackson
-                        </p>
-                    </div>
-
-                    {/* Time */}
-                    <div className=''>
-                        <p>4h 43m</p>
-                    </div>
-
-                    {/* Streak */}
-                    <div className="flex items-center gap-1  justify-center ">
-                        <FaFire className="text-white" />
-                        <span>12 days</span>
-                    </div>
-                </div>
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
-
-
-                    {/* Rank */}
-                    <div>
-                        <p># 2</p>
-                    </div>
-
-                    {/* User */}
-                    <div className="flex items-center gap-2 min-w-0">
-                        <div className="rounded-full size-7 bg-violet-500/30 flex items-center justify-center text-xl">
-                        <p>M</p>
-                        </div>
-
-                        <p className="truncate">
-                        Mike Jackson
-                        </p>
-                    </div>
-
-                    {/* Time */}
-                    <div className=''>
-                        <p>4h 43m</p>
-                    </div>
-
-                    {/* Streak */}
-                    <div className="flex items-center gap-1  justify-center ">
-                        <FaFire className="text-white" />
-                        <span>12 days</span>
-                    </div>
-                </div>
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
-
-
-                    {/* Rank */}
-                    <div>
-                        <p># 2</p>
-                    </div>
-
-                    {/* User */}
-                    <div className="flex items-center gap-2 min-w-0">
-                        <div className="rounded-full size-7 bg-violet-500/30 flex items-center justify-center text-xl">
-                        <p>M</p>
-                        </div>
-
-                        <p className="truncate">
-                        Mike Jackson
-                        </p>
-                    </div>
-
-                    {/* Time */}
-                    <div className=''>
-                        <p>4h 43m</p>
-                    </div>
-
-                    {/* Streak */}
-                    <div className="flex items-center gap-1  justify-center ">
-                        <FaFire className="text-white" />
-                        <span>12 days</span>
-                    </div>
-                </div>
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
-
-
-                    {/* Rank */}
-                    <div>
-                        <p># 2</p>
-                    </div>
-
-                    {/* User */}
-                    <div className="flex items-center gap-2 min-w-0">
-                        <div className="rounded-full size-7 bg-violet-500/30 flex items-center justify-center text-xl">
-                        <p>M</p>
-                        </div>
-
-                        <p className="truncate">
-                        Mike Jackson
-                        </p>
-                    </div>
-
-                    {/* Time */}
-                    <div className=''>
-                        <p>4h 43m</p>
-                    </div>
-
-                    {/* Streak */}
-                    <div className="flex items-center gap-1  justify-center ">
-                        <FaFire className="text-white" />
-                        <span>12 days</span>
-                    </div>
-                </div>
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
-
-
-                    {/* Rank */}
-                    <div>
-                        <p># 2</p>
-                    </div>
-
-                    {/* User */}
-                    <div className="flex items-center gap-2 min-w-0">
-                        <div className="rounded-full size-7 bg-violet-500/30 flex items-center justify-center text-xl">
-                        <p>M</p>
-                        </div>
-
-                        <p className="truncate">
-                        Mike Jackson
-                        </p>
-                    </div>
-
-                    {/* Time */}
-                    <div className=''>
-                        <p>4h 43m</p>
-                    </div>
-
-                    {/* Streak */}
-                    <div className="flex items-center gap-1  justify-center ">
-                        <FaFire className="text-white" />
-                        <span>12 days</span>
-                    </div>
-                </div>
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
-
-
-                    {/* Rank */}
-                    <div>
-                        <p># 2</p>
-                    </div>
-
-                    {/* User */}
-                    <div className="flex items-center gap-2 min-w-0">
-                        <div className="rounded-full size-7 bg-violet-500/30 flex items-center justify-center text-xl">
-                        <p>M</p>
-                        </div>
-
-                        <p className="truncate">
-                        Mike Jackson
-                        </p>
-                    </div>
-
-                    {/* Time */}
-                    <div className=''>
-                        <p>4h 43m</p>
-                    </div>
-
-                    {/* Streak */}
-                    <div className="flex items-center gap-1  justify-center ">
-                        <FaFire className="text-white" />
-                        <span>12 days</span>
-                    </div>
-                </div>
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
-
-
-                    {/* Rank */}
-                    <div>
-                        <p># 2</p>
-                    </div>
-
-                    {/* User */}
-                    <div className="flex items-center gap-2 min-w-0">
-                        <div className="rounded-full size-7 bg-violet-500/30 flex items-center justify-center text-xl">
-                        <p>M</p>
-                        </div>
-
-                        <p className="truncate">
-                        Mike Jackson
-                        </p>
-                    </div>
-
-                    {/* Time */}
-                    <div className=''>
-                        <p>4h 43m</p>
-                    </div>
-
-                    {/* Streak */}
-                    <div className="flex items-center gap-1  justify-center ">
-                        <FaFire className="text-white" />
-                        <span>12 days</span>
-                    </div>
-                </div>
-                <div className="bg-white/2 border-b-2 border-white/10 px-10 py-1 h-15 items-center grid grid-cols-[80px_1fr_195px_135px]">
-
-
-                    {/* Rank */}
-                    <div>
-                        <p># 2</p>
-                    </div>
-
-                    {/* User */}
-                    <div className="flex items-center gap-2 min-w-0">
-                        <div className="rounded-full size-7 bg-violet-500/30 flex items-center justify-center text-xl">
-                        <p>M</p>
-                        </div>
-
-                        <p className="truncate">
-                        Mike Jackson
-                        </p>
-                    </div>
-
-                    {/* Time */}
-                    <div className=''>
-                        <p>4h 43m</p>
-                    </div>
-
-                    {/* Streak */}
-                    <div className="flex items-center gap-1  justify-center ">
-                        <FaFire className="text-white" />
-                        <span>12 days</span>
-                    </div>
-                </div>
 
                 
-                    
+                <div className='px-10 w-full h-20 font-poppins text-neutral-500 text-sm flex items-center justify-center gap-1 flex-col '>
+                    <p>Showing top 100 users.</p>
+                    <p className='text-neutral-600'>Procrastinators were filtered out.</p>
+                </div>
 
             </div>
 
