55d33a69e6a0d5aee1c67071b8fec137cc1b5d01
samiran <sam.de.721166@gmail.com>
2026-06-18 16:00:08 +0530

    Add maintenance page for Timmo V2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.jsx'
+import Maintenance from './Maintenance'
+
+const maintenance = true
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      {maintenance ? <Maintenance/> : <App />}
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
