8b3d8510476ed2deac96e7b46f9e9e4ad1addcfb
Samiran De <sam.de.721166@gmail.com>
2026-06-21 19:19:48 +0530

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import Maintenance from './Maintenance'
 
-const maintenance = false
+const maintenance = true
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
